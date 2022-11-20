b105f61c7448e5209b8262b841c9c10b6fceef49
Sonal Varshney <sonalvrshny@gmail.com>
2022-11-20 17:52:44 -0500

    check base api

--- src/tuiter/home/index.js
+++ src/tuiter/home/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PostHomeList from "../post-home-list/post-home-list";
 import WhatsHappening from "./whats-happening";
 import TuitList from "../tuits/tuit-list";
 
