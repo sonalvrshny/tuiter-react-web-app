3bc33998d4ce96b851ae64f12d0e4d2e8b09ae5c
Sonal Varshney <sonalvrshny@gmail.com>
2022-11-20 17:33:31 -0500

    check base api

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = `${API_BASE}/tuits`;
+const TUITS_API = `${API_BASE}`;
 
 console.log(TUITS_API)
 export const createTuit = async (tuit) => {
